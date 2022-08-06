783dd2f24ced2a14a540401a773c56eb17501610
LexbuIDK <83859654+LexbuIDK@users.noreply.github.com>
2022-08-06 10:15:58 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,18 @@
 
 const items = [
-    { icon: 'fab fa-youtube', link: 'https://youtube.com/UmutBayraktarYT' },
-    { icon: 'fab fa-twitch', link: 'https://twitch.com/umutyzx' },
-    { icon: 'fab fa-discord', link: 'https://discord.com/users/615029465726320654' },
-    { icon: 'fab fa-instagram', link: 'https://instagram.com/umutxyp' },
-    { icon: 'fab fa-facebook', link: 'https://facebook.com/umutxyp' },
-    { icon: 'fab fa-github', link: 'https://github.com/umutxyp' },
+    { icon: 'fab fa-youtube', link: 'https://www.youtube.com/channel/UCGEQ_qp7WRh0o4eS-3CQBRQ' },
+    //{ icon: 'fab fa-twitch', link: 'https://twitch.com/umutyzx' },
+    { icon: 'fab fa-discord', link: 'https://discord.com/users/349217516968738837' },
+    //{ icon: 'fab fa-instagram', link: 'https://instagram.com/umutxyp' },
+    //{ icon: 'fab fa-facebook', link: 'https://facebook.com/umutxyp' },
+    { icon: 'fab fa-github', link: 'https://github.com/lexbuidk' },
   ]
 
 const navItems = [
-    { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
-    { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
-    { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
-    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.umut.live' }
+    { icon: 'fal fa-home', active: 'fa fa-home', label: 'Anasayfa', href: '/' },
+    { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projelerim', href: '/projects' },
+    { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'İletişim', href: '/contact' },
+    //{ icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.umut.live' }
 ]
 
 import Link from 'next/link';
